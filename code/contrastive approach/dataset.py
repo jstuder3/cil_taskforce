7f2e70c692889b274b32571de4fcf3f9d7060194
Justin Studer <jstuder@student.ethz.ch>
2022-06-08 17:56:13 +0200

    Made everything much faster by truncating inputs

--- code/contrastive approach/dataset.py
+++ code/contrastive approach/dataset.py
@@ -1,7 +1,28 @@
 import torch
+import re
+
+def remove_multi_spaces(input_data):
+    out_data = []
+    for elem in input_data:
+        elem=re.sub(" +", " ", elem)
+        out_data+=[elem]
+    return out_data
+
+def replace_usr_and_url(input_data):
+    out_data = []
+    for elem in input_data:
+        elem = re.sub("<user>", "xxuser", elem)
+        elem = re.sub("<url>", "xxurl", elem)
+        out_data+=[elem]
+    return out_data
 
 class TweetDataset(torch.utils.data.Dataset):
     def __init__(self, text_list, labels, tokenizer):
+
+        #preprocess data: <user> is replaced by xxuser, <url> is replaced by xxurl and multi-spaces are removed
+        text_list = replace_usr_and_url(text_list)
+        text_list = remove_multi_spaces(text_list)
+
         #tokenize data and put it into datasets
         self.tokenized_texts = []
         self.attention_masks = []
@@ -10,7 +31,7 @@ class TweetDataset(torch.utils.data.Dataset):
         for i in range(len(text_list)):
             if i% 20000 == 0:
                 print(f"Tokenization progress: {i} / {len(text_list)} ({100*i/float(len(text_list)):.2f}%)")
-            tokenizer_output = tokenizer(text_list[i], padding="max_length", truncation="longest_first")
+            tokenizer_output = tokenizer(text_list[i], padding="max_length", truncation="longest_first", max_length=40)
             self.tokenized_texts.append(tokenizer_output["input_ids"])
             self.attention_masks.append(tokenizer_output["attention_mask"])
 
